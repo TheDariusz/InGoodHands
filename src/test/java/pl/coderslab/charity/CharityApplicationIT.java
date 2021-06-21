f1c7ebcf4ac9139fc665ce587cabd0af1bd624de
TheDariusz <thedariusz@gmail.com>
2021-06-21 23:49:07 +0200

    Initial commit

---
+++ src/test/java/pl/coderslab/charity/CharityApplicationIT.java
@@ -0,0 +1,22 @@
+package pl.coderslab.charity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CharityApplicationIT {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    void shouldCreateWarnMeApplicationBeat() {
+        assertThat(applicationContext.containsBeanDefinition("charityApplication")).isTrue();
+    }
+
+}
